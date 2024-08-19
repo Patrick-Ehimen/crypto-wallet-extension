7e362f6322daf89fc7e8bea9c9708aa30f060d06
Patrick-Ehimen <osepatrick.ehimen77@gmail.com>
2024-08-19 15:40:46 +0100

    Update Home.jsx

--- wallet-frontend/src/components/Home.jsx
+++ wallet-frontend/src/components/Home.jsx
@@ -10,20 +10,29 @@ function Home() {
   return (
     <div className="content">
       <img src={WalletIcons} alt="logo" className="frontPageLogo" />
-      <h4 className="h4"> Welcome to your Web3 Wallet</h4>
-      <h2>Setup your Wallet</h2>
+      <h4
+        className="h4 font-semibold text-white
+      text-[28px] text-left ml-6"
+      >
+        Jump start your crypto portfolio
+      </h4>
+      <h2 className="text-left text-[16px] text-yellow-50">
+        Take your investment portfolio to next level
+      </h2>
 
       <Button
         onClick={() => navigate("/yourwallet")}
         className="frontPageButton"
         type="primary"
+        style={{ backgroundColor: "#6552FE", borderColor: "#6552FE" }}
       >
         Create A Wallet
       </Button>
       <Button
         onClick={() => navigate("/recover")}
-        className="frontPageButton"
+        className="frontPageButton mb-5"
         type="default"
+        style={{ color: "#6552FE" }}
       >
         Sign In With Seed Phrase
       </Button>
