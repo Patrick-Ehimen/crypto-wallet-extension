023611c2de6598f65ea2118b02c6b86a61f19c20
Patrick-Ehimen <osepatrick.ehimen77@gmail.com>
2024-08-19 15:40:08 +0100

    add svg assets

--- wallet-frontend/public/index.js
+++ wallet-frontend/public/index.js
@@ -1,5 +1,19 @@
 import Logo from "./assets/logo.svg";
 import WalletIcons from "./assets/walletIcons.svg";
 import noImg from "./assets/noImg.png";
+import ArbitrumImg from "./assets/arbitrum.svg";
+import BinanceImg from "./assets/BinanceSmartChain.svg";
+import EthereumImg from "./assets/Ethereum.svg";
+import PolygonImg from "./assets/Polygon.svg";
+import EmptyWallet from "./assets/EmptyWallet.svg";
 
-export { Logo, WalletIcons, noImg };
+export {
+  Logo,
+  WalletIcons,
+  noImg,
+  ArbitrumImg,
+  BinanceImg,
+  EthereumImg,
+  PolygonImg,
+  EmptyWallet,
+};
