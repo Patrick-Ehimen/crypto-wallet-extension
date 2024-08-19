bc3e0591ba6a57a14d8e7bb9abbcf1369431102e
Patrick-Ehimen <osepatrick.ehimen77@gmail.com>
2024-08-19 15:40:50 +0100

    Update App.jsx

--- wallet-frontend/src/App.jsx
+++ wallet-frontend/src/App.jsx
@@ -1,7 +1,13 @@
 import "./style/app.css";
 
 import { useState } from "react";
-import { Logo } from "../public";
+import {
+  Logo,
+  EthereumImg,
+  BinanceImg,
+  ArbitrumImg,
+  PolygonImg,
+} from "../public";
 import { Select } from "antd";
 import { Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
@@ -23,20 +29,48 @@ function App() {
           value={selectedChain}
           options={[
             {
-              label: "Ethereum",
+              label: (
+                <div className="option-container my-2 flex">
+                  <img
+                    src={EthereumImg}
+                    alt="Ethereum"
+                    className="mt-1 w-10 h-6"
+                  />
+                  Ethereum
+                </div>
+              ),
               value: "0x1",
             },
+
             {
-              label: "Mumbai Testnet",
-              value: "0x13881",
-            },
-            {
-              label: "Polygon",
+              label: (
+                <div className="option-container my-2 flex">
+                  <img
+                    src={PolygonImg}
+                    alt="Ethereum"
+                    className="mt-1 w-10 h-6"
+                  />
+                  Polygon
+                </div>
+              ),
               value: "0x89",
             },
             {
-              label: "Avalanche",
-              value: "0xa86a",
+              label: (
+                <div className="option-container my-2 flex">
+                  <img
+                    src={ArbitrumImg}
+                    alt="Ethereum"
+                    className="mt-1 w-10 h-6"
+                  />
+                  Arbitrum
+                </div>
+              ),
+              value: "0xa4b1",
+            },
+            {
+              label: "Sepolia Testnet",
+              value: "0xaa36a7",
             },
           ]}
           className="dropdown"
